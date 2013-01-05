4ca6e954c799cf00933f9f1962fcc9b3864e3a05
Jason F. Irwin <jason@j2fi.net>
2013-01-05 22:11:57 +0900

    Version Number Correction
    A new version number system is being put into place, with the following
    format:
    * 2-digit year
    * Alpha Month
    * 3-digit Release Number
    
    10th release in January of 2013 will read: 13A010

--- conf/config.php
+++ conf/config.php
@@ -4,23 +4,23 @@
  * @author Jason F. Irwin
  * @copyright 2012
  */
-define('APP_ROOT', '/');                                // The Application Root Location
-define('APP_NAME', 'Noteworthy');                       // The Application Name
-define('APP_VER', '0.9.0 (2013.01)');                   // The Application Version
-define('CACHE_EXPY', 3600);								// Number of Seconds Cache Files Can Survive
-define('COOKIE_EXPY', 3600);                            // Number of Seconds Mortal Cookies Live For
-define('SHA_SALT', 'nwSiteWith5');                      // Salt Value used with SHA1 Encryption
-define('ENABLE_MULTILANG', 0);							// Enables Multi-Language Support
-define('ANALYTICS_ENABLED', 0);							// Enables the Google Analytics Suffix
+define('APP_ROOT', '/');					// The Application Root Location
+define('APP_NAME', 'Noteworthy');			// The Application Name
+define('APP_VER', '13A001 (2013.01)');		// The Application Version
+define('CACHE_EXPY', 3600);					// Number of Seconds Cache Files Can Survive
+define('COOKIE_EXPY', 3600);				// Number of Seconds Mortal Cookies Live For
+define('SHA_SALT', 'nwSiteWith5');			// Salt Value used with SHA1 Encryption
+define('ENABLE_MULTILANG', 0);				// Enables Multi-Language Support
+define('ANALYTICS_ENABLED', 0);				// Enables the Google Analytics Suffix
 
-define('DEFAULT_LANG', 'EN');                           // Default Language Code
-define('GENERATOR', 'Midori Lite 2.2.0');               // Generator Name
+define('DEFAULT_LANG', 'EN');				// Default Language Code
+define('GENERATOR', 'Midori Lite 2.2.0');	// Generator Name
 
 if( !defined('DB_TYPE') ) {
-	define('DB_TYPE', 2);								// State the Database Type (If Not Already Defined)
+	define('DB_TYPE', 2);					// State the Database Type (If Not Already Defined)
 }
 if( !defined('DEBUG_ENABLED') ) {
-	define('DEBUG_ENABLED', 0);							// Set the Debug Level (If Not Already Defined)
+	define('DEBUG_ENABLED', 0);				// Set the Debug Level (If Not Already Defined)
 }
 
 ?>
